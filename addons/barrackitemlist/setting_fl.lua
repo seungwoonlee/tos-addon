63b7b4f6be850b34a4cfbc727091b09ff1bc7604
seungwoonlee <bluecrck@gmail.com>
2019-09-18 01:23:22 +0900

    업데이트 from iMac

--- addons/barrackitemlist/setting_fl.lua
+++ addons/barrackitemlist/setting_fl.lua
@@ -1,1 +1,1 @@
-return {hideNode={[13]=false,[7]=false,[1]=false,[2]=false,[4]=false,[8]=false,[14]=false,[9]=false,[5]=false,[10]=false,[11]=false,[3]=false,[6]=false,[12]=false,[15]=false},OpenNodeAll=false,col="14"}
+return {hideNode={[13]=false,[7]=false,[1]=false,[2]=false,[4]=false,[8]=false,[15]=false,[9]=false,[5]=false,[10]=false,[3]=false,[11]=false,[6]=false,[12]=false,[14]=false},OpenNodeAll=false,col="14"}
